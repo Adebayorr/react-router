95fd172f430dbe7e820f1368fbcb024f979f6c6d
Adebayorr <raheemadebayorr@gmail.com>
2026-07-17 20:20:31 +0100

    add skeleton to NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,10 @@
-function NotFound() {
+import { Link } from "react-router"
 
+function NotFound() {
     return (
         <>
-            Page Not Found
+            <Link className="go-back-btn" to='/'> Go back</Link>
+            <h2>Page Not Found</h2>
         </>
     )
 }
